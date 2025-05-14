3f5ea6a79ad790517e5178fb2cd43fc42a71e538
JPonchiroli <joaopedroponqueroli@gmail.com>
2025-05-13 21:17:46 -0300

    adicionando forncedor repository

---
+++ backend/src/main/java/com/raffi_estoque/repositories/FornecedorRepository.java
@@ -0,0 +1,9 @@
+package com.raffi_estoque.repositories;
+
+import com.raffi_estoque.entities.Fornecedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer> {
+}
