8134fb58028e7fbb84c63d9829ebfeb5ec6dc241
JPonchiroli <joaopedroponqueroli@gmail.com>
2025-05-29 09:33:04 -0300

    adicionando tela para listar clientes

--- front-end/routes/pages.js
+++ front-end/routes/pages.js
@@ -18,6 +18,11 @@ router.get('/cadastro-fornecedor', (req, res) => {
   res.render('cadastro-fornecedor'); 
 });
 
+router.get('/listar-clientes', (req, res) => {
+  res.render('listar-clientes'); 
+});
+
+
 router.get('/chamar-backend-java', async (req, res) => {
   try {
     const response = await axios.get('http://localhost:8080/api');
@@ -27,5 +32,15 @@ router.get('/chamar-backend-java', async (req, res) => {
     res.status(500).json({ erro: 'Erro ao chamar backend Java' });
   }
 });
+
+router.get('/listar-clientes-backend', async (req, res) => {
+  try {
+    const response = await axios.get('http://localhost:8080/api/clientes/get-all-clientes');
+    res.json(response.data);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ erro: 'Erro ao listar clientes do backend Java' });
+  }
+})
 
 module.exports = router;
