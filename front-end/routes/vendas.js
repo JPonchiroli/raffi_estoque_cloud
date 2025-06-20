a9f069743cbae3f9e327e035c049cd8af4a5186f
JPonchiroli <joaopedroponqueroli@gmail.com>
2025-06-20 10:44:27 -0300

    adicionando deleção de venda

--- front-end/routes/vendas.js
+++ front-end/routes/vendas.js
@@ -4,19 +4,19 @@ const router = express.Router();
 
 
 router.post('/cadastrar-venda-backend', async (req, res) => {
-    const { codCliente, itens } = req.body;
+  const { codCliente, itens } = req.body;
 
-    try {
-        const resposta = await axios.post('http://localhost:8080/api/vendas/create-venda', {
-            codCliente,
-            itens
-        });
+  try {
+    const resposta = await axios.post('http://localhost:8080/api/vendas/create-venda', {
+      codCliente,
+      itens
+    });
 
-        res.status(resposta.status).json({ mensagem: 'Venda cadastrada com sucesso!' });
-    } catch (erro) {
-        console.error('Erro ao enviar venda para o backend:', erro.message);
-        res.status(500).json({ erro: 'Erro ao cadastrar venda' });
-    }
+    res.status(resposta.status).json({ mensagem: 'Venda cadastrada com sucesso!' });
+  } catch (erro) {
+    console.error('Erro ao enviar venda para o backend:', erro.message);
+    res.status(500).json({ erro: 'Erro ao cadastrar venda' });
+  }
 });
 
 router.get('/listar-vendas-backend', async (req, res) => {
@@ -28,5 +28,21 @@ router.get('/listar-vendas-backend', async (req, res) => {
     console.error(error.message);
   }
 });
+
+router.delete('/deletar-venda-backend/:id', async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    return res.status(400).json({ erro: 'ID da venda é obrigatório' });
+  }
+
+  try {
+    const response = await axios.delete(`http://localhost:8080/api/vendas/delete-venda/${id}`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ erro: 'Erro ao deletar venda do backend Java' });
+    console.error(error.message);
+  }
+});
+
 
 module.exports = router;
